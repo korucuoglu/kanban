35cc4f24f27378d9fd49b50fcde8ccd9a681c0b4
Mehmet Oztokay-Mac <mehmetoztokay@gmail.com>
2022-01-03 18:00:17 +0300

    Tab icerik yapisi olusturuldu.

---
+++ src/components/Kanban/TabItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="item">
+    <p class="title">Selam</p>
+    <p class="content">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo cupiditate
+      voluptas quis. Recusandae, corporis quia?
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.item {
+  background-color: blueviolet;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+  .title {
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 10px;
+    line-height: 1.5;
+  }
+  .content {
+    line-height: 1.4;
+    font-size: 14px;
+    opacity: 0.7;
+  }
+}
+</style>
