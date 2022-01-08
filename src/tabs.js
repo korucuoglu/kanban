bc8755c1f683b753cccbf26aa4994f8914f1eebd
Mehmet Öztokay <mehmetoztokay@gmail.com>
2022-01-08 14:55:06 +0300

    Verideki id degerleri duzenlendi.

--- src/tabs.js
+++ src/tabs.js
@@ -1,22 +1,22 @@
 const tabs = [
   {
-    id: 0,
+    id: 1,
     title: 'Backlog',
     items: [
       {
-        id: 0,
+        id: 11,
         title: 'Twilio integration',
         color: '#C340A1',
         content: 'Create new note via SMS. Support text, audio, links, and media.',
       },
       {
-        id: 1,
+        id: 12,
         title: 'Audio recording in note',
         color: '#6A6DCD',
         content: 'Show audio in a note and playback UI',
       },
       {
-        id: 2,
+        id: 13,
         title: 'Markdown support',
         color: '#6A6DCD',
         content: 'Markdown shorthand converts to formatting',
@@ -24,23 +24,23 @@ const tabs = [
     ],
   },
   {
-    id: 1,
+    id: 2,
     title: 'To do',
     items: [
       {
-        id: 0,
+        id: 21,
         title: 'Tablet view',
         color: '#D93535',
         content: '',
       },
       {
-        id: 1,
+        id: 22,
         title: 'Bookmark in note',
         color: '#6A6DCD',
         content: 'Show rich link UI in a note, and feature to render website screenshot.',
       },
       {
-        id: 2,
+        id: 23,
         title: 'Image viewer',
         color: '#6A6DCD',
         content: 'Opens when clicking on the thumbnail in the list or on the image in the note',
@@ -48,29 +48,29 @@ const tabs = [
     ],
   },
   {
-    id: 2,
+    id: 3,
     title: 'In Progress',
     items: [
       {
-        id: 0,
+        id: 31,
         title: 'Mobile view',
         color: '#D93535',
         content: 'Functions for both web responsive and native apps. Note: Android and iOS will need unique share icons.',
       },
       {
-        id: 1,
+        id: 32,
         title: 'Desktop view',
         color: '#D93535',
         content: 'PWA for website and native apps. Note: Windows and Mac will need unique share icons.',
       },
       {
-        id: 2,
+        id: 33,
         title: 'Formatting options',
         color: '#307FE2',
         content: 'Mobile formatting bar expands and collapses when tapping the format icon.',
       },
       {
-        id: 3,
+        id: 34,
         title: 'Media in note',
         color: '#6A6DCD',
         content: 'Image & video with player UI',
@@ -78,23 +78,23 @@ const tabs = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Designed',
     items: [
       {
-        id: 0,
+        id: 41,
         title: 'Audio recording',
         color: '#00A88B',
         content: 'Interface for when recording a new audio note',
       },
       {
-        id: 1,
+        id: 42,
         title: 'Bookmarking',
         color: '#00A88B',
         content: 'Interface for when creating a new link note.',
       },
       {
-        id: 1,
+        id: 43,
         title: 'Mobile home screen',
         color: '#307FE2',
         content: 'Folders, tags, and notes lists are sorted by recent.',
